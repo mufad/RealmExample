c77624520de4e6119e6469161d2c890da20ccd93
Monwar Jahan <mmufad@gmail.com>
2017-01-31 14:23:53 +0600

    done with basic CRUD

--- app/src/main/java/com/droiddigger/realmexample/MainActivity.java
+++ app/src/main/java/com/droiddigger/realmexample/MainActivity.java
@@ -4,14 +4,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Realm realm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Product product=new Product();
-        product.setName("Shirt");
-        product.setPrice("456");
-        Realm realm = Realm.getDefaultInstance();
+        realm = Realm.getDefaultInstance();
+        basicCRUD(realm);
+    }
+    
+    private void basicCRUD(Realm realm){
+        showStatus("BASIC CRUD OPERATION GOING ON");
+         realm.executeTransaction(new Realm.Transaction() {
+        @Override
+        public void execute(Realm realm) {
+                // Add a person
+        
+               Product product=new Product();
+               product.setName("Shirt");
+               product.setPrice("456");
+ 
+        }
+       });
+        
+        final Product product = realm.where(Product.class).findFirst();
+        showStatus(product.getName()+" : "+product.getPrice());
+        
+        realm.executeTransaction(new Realm.Transaction(){
+        
+            @Override
+            public void execute(Realm realm){
+                product.setName("Jeans");
+                product.setPrice("789");
+            }
+        });
+        showStatus(product.getName()+" : "+product.getPrice());
+        
+    }
+    
+    private void showStatus(String text){
+        Log.d("TAG", text);
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
     }
 }
