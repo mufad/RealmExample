00cbffcbdec19bf04250e67279903453a3bee7f5
Monwar Jahan <mmufad@gmail.com>
2017-01-31 13:34:43 +0600

    updated

--- app/src/main/java/com/droiddigger/realmexample/MainActivity.java
+++ app/src/main/java/com/droiddigger/realmexample/MainActivity.java
@@ -9,5 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Product product=new Product();
+        product.setName("Shirt");
+        product.setPrice("456");
+        Realm realm = Realm.getDefaultInstance();
     }
 }
